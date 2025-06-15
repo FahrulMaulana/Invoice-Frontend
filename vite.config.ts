b3f77765343379c4368656dfec370172891b1680
fahrul.maulana <fahrul.it@unpam.ac.id>
2025-06-15 18:39:00 +0700

    port -> 8080

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,10 @@ import { defineConfig } from "vite";
 export default defineConfig({
   plugins: [react()],
   server: {
+    port: 8080,
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "http://localhost:3001",
         changeOrigin: true,
         secure: false,
       },
